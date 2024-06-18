55aca2305a0d2e557f58795d14e63c7af7ad87f9
AmitStredz <ponnan2112@gmail.com>
2024-06-18 19:16:18 +0530

    addons

--- src/views/homepage/header.js
+++ src/views/homepage/header.js
@@ -5,11 +5,11 @@ export default function Header() {
   const navigate = useNavigate();
 
   return (
-    <div className="w-screen h-28 top-0 flex justify-between p-5 px-20">
+    <div className="w-screen h-28 top-0 flex justify-between p-5 px-20 text-white">
       <div>
         <a className="text-[30px] font-bold cursor-pointer" onClick={() => navigate('/homePage')}>Visify</a>
       </div>
-      <div className="flex gap-16">
+      <div className="flex gap-16 items-center">
         <a className="cursor-pointer">About</a>
         <a className="cursor-pointer">Services</a>
         <a className="cursor-pointer">Country</a>

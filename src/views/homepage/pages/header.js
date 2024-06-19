09c71ea3b9e59f83d8e9d5c7d2b1c0e715825775
AmitStredz <ponnan2112@gmail.com>
2024-06-19 22:50:52 +0530

    apply page theme updated

--- src/views/homepage/pages/header.js
+++ src/views/homepage/pages/header.js
@@ -6,7 +6,7 @@ export default function Header() {
   const navigate = useNavigate();
 
   return (
-    <div className="w-screen h-24 top-0 flex gap-5 justify-between p-5 px-20 text-black">
+    <div className="w-screen h-24 top-0 flex gap-5 justify-between p-5 px-20 text-black bg-slate-200">
       <div className="flex items-center cursor-pointer">
         <img src={turkeyLogo} className="w-12"></img>
         <a className="text-[30px] font-bold text-red-600" onClick={() => navigate('/homePage')}>Visify</a>
@@ -25,7 +25,7 @@ export default function Header() {
       <div className="flex items-center gap-5">
         <div className="border-2 border-slate-500 p-1 px-2 flex gap-2">
         <i className="ri-search-line"></i>
-        <input className=" outline-none w-60" type="text" placeholder="Search"/>
+        <input className=" outline-none w-60 bg-transparent" type="text" placeholder="Search"/>
 
         </div>
         <button className="p-2 px-4 border border-slate-800  bg-red-600 bg-opacity-80 text-white">

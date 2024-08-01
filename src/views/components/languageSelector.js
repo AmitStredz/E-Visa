339cca0fb1ed629ea098578c1639270b721b795a
AmitStredz <ponnan2112@gmail.com>
2024-08-01 19:51:02 +0530

    header changed

--- src/views/components/languageSelector.js
+++ src/views/components/languageSelector.js
@@ -36,6 +36,13 @@
 
 import React from "react";
 
+// import turkeyFlag from '../assets/turkeyFlag.webp';
+// import IndiaFlag from '../assets/indiaFlag.webp';
+// import ChinaFlag from '../assets/chinaFlag.webp';
+// import ArabicFlag from '../assets/arabicFlag.webp';
+// import GermanFlag from '../assets/germanFlag.webp';
+// import EspanolFlag from '../assets/espanolFlag.webp';
+
 const LanguageSelector = () => {
   const handleLanguageChange = (lang) => {
     const selectField = document.querySelector("select.goog-te-combo");
@@ -49,18 +56,80 @@ const LanguageSelector = () => {
     { code: "en", name: "English" },
     { code: "fr", name: "Français" },
     { code: "es", name: "Español" },
-    // Add more languages as needed
+    { code: "de", name: "Deutsch" }, // German
+    { code: "zh-CN", name: "简体中文" }, // Simplified Chinese
+    { code: "zh-TW", name: "繁體中文" }, // Traditional Chinese
+    { code: "ar", name: "العربية" }, // Arabic
+    { code: "hi", name: "हिन्दी" }, // Hindi
+    { code: "tr", name: "Türkçe" }, // Turkish
   ];
+
   return (
-    <select
-      className="custom-language-selector"
-      onChange={(e) => handleLanguageChange(e.target.value)}
-    >
-      <option value="en">English</option>
-      <option value="fr">Français</option>
-      <option value="es">Español</option>
-      <option value="hi">हिन्दी</option>
-    </select>
+    <div className="flex gap-2 flex-wrap items-center justify-center w-full">
+      <button
+        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
+        onClick={() => handleLanguageChange('en')}
+      >
+        <img src="/assets/americaFlag.webp" className="w-5"></img>
+        English
+      </button>
+      <button
+        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
+        onClick={() => handleLanguageChange("English")}
+      >
+        <img src="/assets/arabicFlag.webp" className="w-5"></img>
+        العربية
+      </button>
+      <button
+        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
+        onClick={() => handleLanguageChange('fr')}
+      >
+        <img src="/assets/frenchFlag.webp" className="w-5"></img>
+        Français
+      </button>
+      <button
+        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
+        onClick={() => handleLanguageChange("Deutsch")}
+      >
+        <img src="/assets/germanFlag.webp" className="w-5"></img>
+        Deutsch
+      </button>
+      <button
+        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
+        onClick={() => handleLanguageChange("es")}
+      >
+        <img src="/assets/espanolFlag.webp" className="w-5"></img>
+        Español
+      </button>
+      <button
+        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
+        onClick={() => handleLanguageChange("简体中文")}
+      >
+        <img src="/assets/chinaFlag.webp" className="w-5"></img>
+        简体中文
+      </button>
+      <button
+        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
+        onClick={() => handleLanguageChange("繁體中文")}
+      >
+        <img src="/assets/chinaFlag.webp" className="w-5"></img>
+        繁體中文
+      </button>
+      <button
+        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
+        onClick={() => handleLanguageChange("hi")}
+      >
+        <img src="/assets/indiaFlag.webp" className="w-5"></img>
+        हिन्दी
+      </button>
+      <button
+        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
+        onClick={() => handleLanguageChange("Türkçe")}
+      >
+        <img src="/assets/turkeyFlag.webp" className="w-5"></img>
+        Türkçe
+      </button>
+    </div>
   );
 };
 
--- src/views/homepage/pages/header.js
+++ src/views/homepage/pages/header.js
@@ -1,84 +1,44 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import turkeyLogo from "../assets/logo2.png";
-import { IoIosMenu } from "react-icons/io";
-import { IoClose } from "react-icons/io5";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
+
 
 import LanguageSelector from "../../components/languageSelector";
 
 export default function Header() {
-  const [isMenu, setisMenu] = useState(false);
   const navigate = useNavigate();
 
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
   return (
     <div className="w-screen h-24 top-0 flex gap-2 sm:gap-5 justify-between p-2 sm:p-5  sm:px-10 xl:px- text-black bg-slate-200">
-      <div
-        className="flex items-center cursor-pointer w-full"
-        onClick={() => navigate("/homepage")}
-      >
-        <img src={turkeyLogo} className="w-14"></img>
-        <a
-          className="text-[16px] sm:text-[20px] font-bold text-red-600"
-          onClick={() => navigate("/homePage")}
-        >
-          e-Visa for Online Application
-        </a>
-      </div>
-
-      <div className="hidden lg:flex gap-5 xl:gap-10 items-center text-[18px]">
-        <a className="cursor-pointer">About</a>
-        <a className="cursor-pointer">Services</a>
-        <a className="cursor-pointer">Country</a>
-        <a className="cursor-pointer">Visa</a>
-        <a className="cursor-pointer">Blog</a>
-      </div>
-      <div className="flex items-center justify-center gap-2 sm:gap-5 w-full">
-        <div className="hidden md:flex border-2 border-slate-500 p-1 px-2  gap-2">
-          <i className="ri-search-line"></i>
-          <input
-            className=" outline-none w-60 bg-transparent"
-            type="text"
-            placeholder="Search"
-          />
-        </div>
+      <div className="flex gap-2 items-center w-full">
         <div
-          id="google_translate_element"
-          // className="w-20 overflow-hidden object-contain"
-        ></div>
-        <div className="flex flex-col sm:flex-row justify-center items-center gap-2 w-full">
-          <button className="flex p-1 sm:p-2 sm:px-4 border border-slate-800  bg-red-600 bg-opacity-80 text-white">
-            Contact Us
-          </button>
-          <LanguageSelector />
+          className="flex items-center cursor-pointer"
+          onClick={() => navigate("/homepage")}
+        >
+          <img src={turkeyLogo} className="w-14"></img>
+          <a
+            className="text-[16px] sm:text-[20px] font-bold text-red-600"
+            // onClick={() => navigate("/homePage")}
+          >
+            e-Visa
+          </a>
         </div>
-        <IoIosMenu
-          size={30}
-          className="lg:hidden"
-          onClick={() => setisMenu(true)}
-        />
+        <div className="h-full w-[2px] bg-black"></div>
+        <div className="flex flex-col">
+          <span className="font-semibold font-serif text-[28px] text-red-500">
+            Turkey Visa
+          </span>
+          <span className="font-semibold font-serif text-[20px] text-red-500">
+            Online Application System
+          </span>
+        </div>
       </div>
 
-      {/* <LanguageSelector></LanguageSelector> */}
-
-      <div
-        className={`fixed flex flex-col w-[50%]  items-center p-5 h-screen backdrop:blur-md top-0 lg:hidden bg-white gap-5 transition-all ${
-          isMenu ? "right-0" : "hidden right-[100%]"
-        }`}
-        // data-aos="fade-left"
-      >
-        <div className="flex w-full p-2 justify-start">
-          <IoClose size={40} onClick={() => setisMenu(false)} />
-        </div>
-        <div className="flex flex-col justify-evenly h-[50%]">
-          <a className="cursor-pointer">About</a>
-          <a className="cursor-pointer">Services</a>
-          <a className="cursor-pointer">Country</a>
-          <a className="cursor-pointer">Visa</a>
-          <a className="cursor-pointer">Blog</a>
-        </div>
+      <div className="w-full">
+        <LanguageSelector />
       </div>
     </div>
   );

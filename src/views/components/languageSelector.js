69e931a5cac499e3162aa1772b62b463360e73d6
AmitStredz <ponnan2112@gmail.com>
2024-08-01 20:33:17 +0530

    addons
    updated header and language changer

--- src/views/components/languageSelector.js
+++ src/views/components/languageSelector.js
@@ -34,7 +34,7 @@
 
 // export default LanguageSelector;
 
-import React from "react";
+import React, { useState } from "react";
 
 // import turkeyFlag from '../assets/turkeyFlag.webp';
 // import IndiaFlag from '../assets/indiaFlag.webp';
@@ -43,8 +43,12 @@ import React from "react";
 // import GermanFlag from '../assets/germanFlag.webp';
 // import EspanolFlag from '../assets/espanolFlag.webp';
 
-const LanguageSelector = () => {
+const LanguageSelector = ({onclose}) => {
+  const [isSelected, setISelected] = useState("en");
+
   const handleLanguageChange = (lang) => {
+    onclose();
+    setISelected(lang);
     const selectField = document.querySelector("select.goog-te-combo");
     if (selectField) {
       selectField.value = lang;
@@ -52,79 +56,115 @@ const LanguageSelector = () => {
     }
   };
 
-  const languages = [
-    { code: "en", name: "English" },
-    { code: "fr", name: "Français" },
-    { code: "es", name: "Español" },
-    { code: "de", name: "Deutsch" }, // German
-    { code: "zh-CN", name: "简体中文" }, // Simplified Chinese
-    { code: "zh-TW", name: "繁體中文" }, // Traditional Chinese
-    { code: "ar", name: "العربية" }, // Arabic
-    { code: "hi", name: "हिन्दी" }, // Hindi
-    { code: "tr", name: "Türkçe" }, // Turkish
-  ];
+  // const languages = [
+  //   { code: "en", name: "English" },
+  //   { code: "fr", name: "Français" },
+  //   { code: "es", name: "Español" },
+  //   { code: "de", name: "Deutsch" }, // German
+  //   { code: "zh-CN", name: "简体中文" }, // Simplified Chinese
+  //   { code: "zh-TW", name: "繁體中文" }, // Traditional Chinese
+  //   { code: "ar", name: "العربية" }, // Arabic
+  //   { code: "hi", name: "हिन्दी" }, // Hindi
+  //   { code: "tr", name: "Türkçe" }, // Turkish
+  // ];
 
   return (
-    <div className="flex gap-2 flex-wrap items-center justify-center w-full">
+    <div className="flex gap-1 flex-wrap items-center justify-center w-full">
       <button
-        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
-        onClick={() => handleLanguageChange('en')}
+        className={`flex items-center gap-1 p-1 px-4 border rounded-lg hover:border-slate-500 transition-all ${
+          isSelected == "en"
+            ? "border-slate-600 bg-black bg-opacity-30 "
+            : "border-slate-400"
+        }`}
+        onClick={() => handleLanguageChange("en")}
       >
         <img src="/assets/americaFlag.webp" className="w-5"></img>
         English
       </button>
       <button
-        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
-        onClick={() => handleLanguageChange("English")}
+        className={`flex items-center gap-1 p-1 px-4 border rounded-lg hover:border-slate-500 transition-all ${
+          isSelected == "ar"
+            ? "border-slate-600 bg-black bg-opacity-30 "
+            : "border-slate-400"
+        }`}
+        onClick={() => handleLanguageChange("ar")}
       >
         <img src="/assets/arabicFlag.webp" className="w-5"></img>
         العربية
       </button>
       <button
-        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
-        onClick={() => handleLanguageChange('fr')}
+        className={`flex items-center gap-1 p-1 px-4 border rounded-lg hover:border-slate-500 transition-all ${
+          isSelected == "fr"
+            ? "border-slate-600 bg-black bg-opacity-30 "
+            : "border-slate-400"
+        }`}
+        onClick={() => handleLanguageChange("fr")}
       >
         <img src="/assets/frenchFlag.webp" className="w-5"></img>
         Français
       </button>
       <button
-        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
-        onClick={() => handleLanguageChange("Deutsch")}
+        className={`flex items-center gap-1 p-1 px-4 border rounded-lg hover:border-slate-500 transition-all ${
+          isSelected == "de"
+            ? "border-slate-600 bg-black bg-opacity-30 "
+            : "border-slate-400"
+        }`}
+        onClick={() => handleLanguageChange("de")}
       >
         <img src="/assets/germanFlag.webp" className="w-5"></img>
         Deutsch
       </button>
       <button
-        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
+        className={`flex items-center gap-1 p-1 px-4 border rounded-lg hover:border-slate-500 transition-all ${
+          isSelected == "es"
+            ? "border-slate-600 bg-black bg-opacity-30 "
+            : "border-slate-400"
+        }`}
         onClick={() => handleLanguageChange("es")}
       >
         <img src="/assets/espanolFlag.webp" className="w-5"></img>
         Español
       </button>
       <button
-        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
-        onClick={() => handleLanguageChange("简体中文")}
+        className={`flex items-center gap-1 p-1 px-4 border rounded-lg hover:border-slate-500 transition-all ${
+          isSelected == "zh-CN"
+            ? "border-slate-600 bg-black bg-opacity-30 "
+            : "border-slate-400"
+        }`}
+        onClick={() => handleLanguageChange("zh-CN")}
       >
         <img src="/assets/chinaFlag.webp" className="w-5"></img>
         简体中文
       </button>
       <button
-        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
-        onClick={() => handleLanguageChange("繁體中文")}
+        className={`flex items-center gap-1 p-1 px-4 border rounded-lg hover:border-slate-500 transition-all ${
+          isSelected == "zh-TW"
+            ? "border-slate-600 bg-black bg-opacity-30 "
+            : "border-slate-400"
+        }`}
+        onClick={() => handleLanguageChange("zh-TW")}
       >
         <img src="/assets/chinaFlag.webp" className="w-5"></img>
         繁體中文
       </button>
       <button
-        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
+        className={`flex items-center gap-1 p-1 px-4 border rounded-lg hover:border-slate-500 transition-all ${
+          isSelected == "hi"
+            ? "border-slate-600 bg-black bg-opacity-30 "
+            : "border-slate-400"
+        }`}
         onClick={() => handleLanguageChange("hi")}
       >
         <img src="/assets/indiaFlag.webp" className="w-5"></img>
         हिन्दी
       </button>
       <button
-        className="flex items-center gap-1 p-1 px-4 border border-slate-400 rounded-lg hover:border-slate-500 transition-all"
-        onClick={() => handleLanguageChange("Türkçe")}
+        className={`flex items-center gap-1 p-1 px-4 border rounded-lg hover:border-slate-500 transition-all ${
+          isSelected == "tr"
+            ? "border-slate-600 bg-black bg-opacity-30 "
+            : "border-slate-400"
+        }`}
+        onClick={() => handleLanguageChange("tr")}
       >
         <img src="/assets/turkeyFlag.webp" className="w-5"></img>
         Türkçe
--- src/views/homepage/pages/header.js
+++ src/views/homepage/pages/header.js
@@ -3,19 +3,20 @@ import { useNavigate } from "react-router";
 import turkeyLogo from "../assets/logo2.png";
 // import { useTranslation } from "react-i18next";
 
-
 import LanguageSelector from "../../components/languageSelector";
+import { IoClose } from "react-icons/io5";
 
 export default function Header() {
+  const [isGlobe, setIsGlobe] = useState(false);
   const navigate = useNavigate();
 
   // const { t } = useTranslation();
 
   return (
-    <div className="w-screen h-24 top-0 flex gap-2 sm:gap-5 justify-between p-2 sm:p-5  sm:px-10 xl:px- text-black bg-slate-200">
+    <div className="w-screen h-24 top-0 flex gap-2 sm:gap-5 justify-between p-5 sm:px-10 xl:px- text-black bg-slate-200">
       <div className="flex gap-2 items-center w-full">
         <div
-          className="flex items-center cursor-pointer"
+          className="flex flex-col sm:flex-row items-center cursor-pointer"
           onClick={() => navigate("/homepage")}
         >
           <img src={turkeyLogo} className="w-14"></img>
@@ -26,20 +27,40 @@ export default function Header() {
             e-Visa
           </a>
         </div>
-        <div className="h-full w-[2px] bg-black"></div>
+        <div className="h-full w-[2px] bg-red-400"></div>
         <div className="flex flex-col">
-          <span className="font-semibold font-serif text-[28px] text-red-500">
+          <span className="font-semibold font-serif text-[18px] sm:text-[28px] text-red-500">
             Turkey Visa
           </span>
-          <span className="font-semibold font-serif text-[20px] text-red-500">
+          <span className="font-semibold font-serif text-[14px] sm:text-[20px] text-red-500">
             Online Application System
           </span>
         </div>
       </div>
 
-      <div className="w-full">
+      <div className="w-full hidden lg:flex">
         <LanguageSelector />
+        <div id="google_translate_element"></div>
       </div>
+      <img
+        src="/globe.svg"
+        onClick={() => setIsGlobe(!isGlobe)}
+        className="cursor-pointer w-14"
+      ></img>
+
+      {isGlobe && (
+        <div className="absolute flex justify-center items-center p-6 top-0 left-0 h-screen bg-black bg-opacity-40 w-screen">
+          <div className="flex flex-col items-center md:w-[60%] xl:w-[30%] bg-white sm:p-10">
+            <div className="flex justify-end w-full">
+              <IoClose size={40} onClick={() => setIsGlobe(false)} className="cursor-pointer" />
+            </div>
+            <img src="/globe.svg" className="w-20 sm:w-40"></img>
+            <div className="p-5">
+              <LanguageSelector onclose={()=>setIsGlobe(false)}/>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }

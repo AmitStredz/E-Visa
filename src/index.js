4eef3b487fe7ed1d73c68569a09cfff9ed6fc985
AmitStredz <ponnan2112@gmail.com>
2024-08-01 23:57:57 +0530

    addons
    removed i18next dependencies

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './i18n.js'; // Import the i18n configuration
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,34 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-document.addEventListener('DOMContentLoaded', function () {
-  // Function to remove Google Translate elements
-  function removeGoogleTranslateElements() {
-      const translateTooltip = document.querySelector('.goog-te-banner-frame');
-      const translateTooltipContainer = document.querySelector('.goog-tooltip');
-      const translateHighlight = document.querySelector('.goog-text-highlight');
-
-      if (translateTooltip) translateTooltip.style.display = 'none';
-      if (translateTooltipContainer) translateTooltipContainer.style.display = 'none';
-      if (translateHighlight) translateHighlight.style.display = 'none';
-  }
-
-  // Initial call to remove elements
-  removeGoogleTranslateElements();
-
-  // Observer to watch for added nodes and remove them if necessary
-  const observer = new MutationObserver(function (mutations) {
-      mutations.forEach(function (mutation) {
-          if (mutation.addedNodes.length) {
-              removeGoogleTranslateElements();
-          }
-      });
-  });
-
-  // Configure the observer to watch for changes in the document
-  observer.observe(document.body, {
-      childList: true,
-      subtree: true
-  });
-});
--- src/views/homepage/pages/header.js
+++ src/views/homepage/pages/header.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import turkeyLogo from "../assets/logo2.png";
-// import { useTranslation } from "react-i18next";
 
 import LanguageSelector from "../../components/languageSelector";
 import { IoClose } from "react-icons/io5";
@@ -9,8 +8,6 @@ import { IoClose } from "react-icons/io5";
 export default function Header() {
   const [isGlobe, setIsGlobe] = useState(false);
   const navigate = useNavigate();
-
-  // const { t } = useTranslation();
 
   return (
     <div className="relative w-screen h-24 top-0 flex gap-2 sm:gap-5 justify-between p-5 sm:px-10 xl:px- text-black bg-slate-200 z-[1000000000]">
